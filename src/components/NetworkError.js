1e64520cc73fd6838f5d1479c05a5e362bf6560b
dupreeh <beddenis13@gmail.com>
2019-12-29 11:31:01 +0200

    Add files via upload

--- src/components/NetworkError.js
+++ src/components/NetworkError.js
@@ -15,7 +15,7 @@ function NetworkError(props) {
       >
         More about mixed content.
       </a>
-      <div style={{ margin: "10px 0px" }}>
+      <div style={{ margin: "40px 0px" }}>
         <GifGuide></GifGuide>
       </div>
     </div>
