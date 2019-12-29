fd7274c7b25e7d23e0373561edbbdb058d6e763a
dupreeh <beddenis13@gmail.com>
2019-12-29 11:26:56 +0200

    Add files via upload

--- src/components/NetworkError.js
+++ src/components/NetworkError.js
@@ -7,14 +7,14 @@ function NetworkError(props) {
         This is not your fault! Please, enable mixed content to get rid of this
         error. This error occurs because i use OpenWeather MAP API that uses
         HTTP instead of HTTPS.
-        <br></br>
-        <a
-          className="highlighted-link"
-          href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content"
-        >
-          More about mixed content.
-        </a>
       </p>
+      <br />
+      <a
+        className="highlighted-link"
+        href="https://developers.google.com/web/fundamentals/security/prevent-mixed-content/what-is-mixed-content"
+      >
+        More about mixed content.
+      </a>
       <div style={{ margin: "10px 0px" }}>
         <GifGuide></GifGuide>
       </div>
